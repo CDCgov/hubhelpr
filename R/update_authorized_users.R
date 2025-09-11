ce3e0c3b2e88bc58a0566b67e1507d77c65ecd7d
O957 <127630341+O957@users.noreply.github.com>
2025-09-11 11:16:59 -0400

    use load metadata

--- R/update_authorized_users.R
+++ R/update_authorized_users.R
@@ -2,39 +2,45 @@
 #'
 #' This function reads YAML metadata files from a Hub's
 #' model-metadata directory and extracts authorized GitHub
-# users for each model, saving the results to a JSON file in the hub `auxiliary-data` directory.
-# JSON file.
+#' users for each model, saving the results to a JSON file
+#' in the hub `auxiliary-data` directory.
+#' JSON file.
 #'
 #' @param base_hub_path Path to the base hub directory.
-#' @return `NULL`, invisibly. Writes `authorized_users.json` file to the
-#' `auxiliary-data` directory of the hub as a side effect.
+#' @return `NULL`, invisibly. Writes
+#' `authorized_users.json` file to the `auxiliary-data`
+#' directory of the hub as a side effect.
 #' @export
 update_authorized_users <- function(base_hub_path) {
   output_dir <- fs::path(base_hub_path, "auxiliary-data")
-  metadata_dir <- fs::path(base_hub_path, "model-metadata")
 
   fs::dir_create(output_dir)
 
-  yml_files <- fs::dir_ls(
-    metadata_dir,
-    glob = "*.{yml,yaml}"
-  )
+  model_metadata <- hubData::load_model_metadata(base_hub_path)
 
-  json_list <- purrr::map(yml_files, \(file) {
-    y <- yaml::read_yaml(file)
-    team <- if (!is.null(y$team_abbr)) y$team_abbr else NA_character_
-    model <- if (!is.null(y$model_abbr)) y$model_abbr else NA_character_
-    designated_users <- if (!is.null(y$designated_github_users)) {
-      # ensure single-user lists are rendered as JSON lists
-      I(y$designated_github_users)
-    } else {
-      NA
-    }
+  json_list <- purrr::pmap(
     list(
-      model = paste(team, model, sep = "-"),
-      authorized_github_users = designated_users
-    )
-  })
+      team = model_metadata$team_abbr,
+      model = model_metadata$model_abbr,
+      designated_users = model_metadata$designated_github_users
+    ),
+    \(team, model, designated_users) {
+      team <- if (!is.null(team) && !is.na(team)) team else NA_character_
+      model <- if (!is.null(model) && !is.na(model)) model else NA_character_
+      designated_users <- if (
+        !is.null(designated_users) && !all(is.na(designated_users))
+      ) {
+        # ensure single-user lists are rendered as JSON lists
+        I(designated_users)
+      } else {
+        NA
+      }
+      list(
+        model = paste(team, model, sep = "-"),
+        authorized_github_users = designated_users
+      )
+    }
+  )
 
   jsonlite::write_json(
     json_list,
