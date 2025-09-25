0f2727cfd103e44965f421ca33ae63b57a0fa08f
O957 <127630341+O957@users.noreply.github.com>
2025-09-25 15:03:56 -0400

    add hub_name; not sure how this was deleted

--- R/generate_hub_ensemble.R
+++ R/generate_hub_ensemble.R
@@ -99,7 +99,8 @@ generate_hub_ensemble <- function(
     )
   }
 
-  ensemble_model_name <- glue::glue("{get_hub_name(disease)}-ensemble")
+  hub_name <- get_hub_name(disease)
+  ensemble_model_name <- glue::glue("{hub_name}-ensemble")
 
   output_dirpath <- fs::path(base_hub_path, "model-output", ensemble_model_name)
   output_filename <- glue::glue("{reference_date}-{hub_name}-ensemble")
