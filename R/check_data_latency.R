327c0f1e111353e5a7f4d62515aa97d0b5aabd9a
sbidari <zib2@cdc.gov>
2025-12-22 11:54:53 -0700

    standalone file for check_data_latency function

---
+++ R/check_data_latency.R
@@ -0,0 +1,76 @@
+#' Checks latency of input data.
+#'
+#' @param data Data frame containing target data.
+#' @param location_col_name Character. Name of the location column.
+#' Default is "geo_value".
+#' @param date_col_name Character. Name of the date column.
+#' Default is "time_value".
+#' @param target_label Character. Human-readable label for the target.
+#' @param reference_date Date. The reference date for forecast.
+#' Default is the current MMWR epiweek ceiling.
+#' @param expected_latency Integer. Expected data latency in days.
+#' Default is 1 week (7 days).
+#' @param desired_max_time_value Date. The most recent date for which
+#' observations are expected. Default is number of days specified by
+#' `expected_latency` before the reference_date.
+#' @param overlatent_err_thresh Numeric. Proportion threshold for raising error
+#' vs warning. Default 0.20.
+#'
+#' @return Invisible NULL. Raises warnings or errors
+#' based on the proportion of locations with excess latency.
+#' @export
+check_data_latency <- function(
+  data,
+  location_col_name = "geo_value",
+  date_col_name = "time_value",
+  target_label,
+  reference_date = NULL,
+  expected_latency = 7,
+  desired_max_time_value = NULL,
+  overlatent_err_thresh = 0.20
+) {
+  if (is.null(reference_date)) {
+    reference_date <- forecasttools::ceiling_mmwr_epiweek(lubridate::today())
+  }
+  if (is.null(desired_max_time_value)) {
+    desired_max_time_value <- reference_date - lubridate::days(expected_latency)
+  }
+
+  latency_tbl <- data |>
+    dplyr::group_by(.data[[location_col_name]]) |>
+    dplyr::summarise(
+      latest_date = max(.data[[date_col_name]], na.rm = TRUE),
+      .groups = "drop"
+    ) |>
+    dplyr::mutate(
+      excess_latency = pmax(
+        as.integer(desired_max_time_value - .data$latest_date) %/% 7L,
+        0L
+      ),
+      has_excess_latency = .data$excess_latency > 0L
+    )
+
+  prop_locs_overlatent <- mean(latency_tbl$has_excess_latency)
+
+  if (prop_locs_overlatent > overlatent_err_thresh) {
+    cli::cli_abort(
+      c(
+        "{target_label}: More than {100 * overlatent_err_thresh}% of locations have excess latency.",
+        "!" = "Reference date: {reference_date}",
+        "!" = "Expected observations through: {desired_max_time_value}",
+        "!" = "{nrow(latency_tbl |> dplyr::filter(.data$has_excess_latency))} location{?s} have excess latency"
+      )
+    )
+  } else if (prop_locs_overlatent > 0) {
+    cli::cli_warn(
+      c(
+        "{target_label}: Some locations have excess latency.",
+        "!" = "Reference date: {reference_date}",
+        "!" = "Expected observations through: {desired_max_time_value}",
+        "!" = "{nrow(latency_tbl |> dplyr::filter(.data$has_excess_latency))} location{?s} have excess latency"
+      )
+    )
+  }
+  return(invisible())
+}
+
