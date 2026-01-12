f99920337ed0968bf08f1dbc1739731f2c435286
sbidari <zib2@cdc.gov>
2026-01-12 14:59:43 -0800

    add function

--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,20 @@ get_hub_name <- function(disease) {
     disease == "rsv" ~ "RSVHub"
   ))
 }
+
+#' Get GitHub repository path for a given disease.
+#'
+#' Converts disease identifier to corresponding GitHub repository path.
+#'
+#' @param disease Character. Disease identifier ("covid" or "rsv").
+#' @return Character. GitHub repository path.
+#' @export
+get_hub_repo <- function(disease) {
+  checkmate::assert_scalar(disease)
+  checkmate::assert_names(disease, subset.of = c("covid", "rsv"))
+
+  return(dplyr::case_when(
+    disease == "covid" ~ "covid19-forecast-data",
+    disease == "rsv" ~ "rsv-forecast-hub"
+  ))
+}
