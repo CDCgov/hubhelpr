8785be7cd379fa38d9ec82d7b0b76c6a7f604d35
sbidari <zib2@cdc.gov>
2026-01-12 16:27:12 -0800

    add population data

---
+++ data-raw/population_data.R
@@ -0,0 +1,7 @@
+population_data <- readr::read_csv(
+  fs::path("inst", "extdata", "locations_with_2023_census_pop.csv"),
+  show_col_types = FALSE
+) |>
+  dplyr::select("location", "population")
+
+  usethis::use_data(population_data, overwrite = TRUE)
