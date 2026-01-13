76a998438980c3bd16a72fd2bc7132e3aa454b11
Subekshya Bidari <37636707+sbidari@users.noreply.github.com>
2026-01-13 11:20:15 -0800

    add population data (#105)
    * add population data
    
    * update deafult in write_red_date_summary_* functions
    
    * Update data-raw/population_data.R
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update R/write_ref_date_summary.R
    
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Population Data by Location
+#'
+#' A dataset containing population information for US states and national level based on
+#' 2023 census data.
+#'
+#' @format A data frame with 2 variables:
+#' \describe{
+#'   \item{location}{Location identifier, two digits FIPS codes}
+#'   \item{population}{Population count from 2023 census}
+#' }
+#' @source data-raw/population_data.R
+"population_data"
