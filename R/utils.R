97e72c8dccb43f69d9d48a86ce8b6695fa4cf7ed
Subekshya Bidari <37636707+sbidari@users.noreply.github.com>
2026-01-14 10:29:47 -0800

    change output folder path in ref date summary and web text generation (#103)
    * add function
    
    * update path
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Apply suggestions from code review
    
    Co-authored-by: O957 <127630341+O957@users.noreply.github.com>
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>
    
    * run devtools
    
    * path -> name
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: O957 <127630341+O957@users.noreply.github.com>
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,7 @@
-#' Get hub name for a given disease.
+#' Get hub display name for a given disease.
 #'
-#' Converts disease identifier to proper hub name format.
+#' Converts disease identifier to hub display name format used for
+#' identifying hub-baseline and hub-ensemble.
 #'
 #' @param disease Character. Disease identifier ("covid" or "rsv").
 #' @return Character. Hub name (e.g., "CovidHub", "RSVHub").
@@ -12,5 +13,22 @@ get_hub_name <- function(disease) {
   return(dplyr::case_when(
     disease == "covid" ~ "CovidHub",
     disease == "rsv" ~ "RSVHub"
+  ))
+}
+
+#' Get GitHub repository name for a given disease.
+#'
+#' Converts disease identifier to corresponding GitHub repository name.
+#'
+#' @param disease Character. Disease identifier ("covid" or "rsv").
+#' @return Character. GitHub repository name.
+#' @export
+get_hub_repo_name <- function(disease) {
+  checkmate::assert_scalar(disease)
+  checkmate::assert_names(disease, subset.of = c("covid", "rsv"))
+
+  return(dplyr::case_when(
+    disease == "covid" ~ "covid19-forecast-hub",
+    disease == "rsv" ~ "rsv-forecast-hub"
   ))
 }
