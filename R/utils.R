f7f49a4af4d2271200c4e59de107333e7af41935
sbidari <zib2@cdc.gov>
2026-01-14 10:52:37 -0800

    remove duplicate

--- R/utils.R
+++ R/utils.R
@@ -16,24 +16,6 @@ get_hub_name <- function(disease) {
   )
 }
 
-
-#' Get hub repository name for a given disease.
-#'
-#' Converts disease identifier to GitHub repository name.
-#'
-#' @param disease Character. Disease identifier ("covid" or "rsv").
-#' @return Character. Repository name.
-#' @export
-get_hub_repo <- function(disease) {
-  checkmate::assert_scalar(disease)
-  checkmate::assert_names(disease, subset.of = c("covid", "rsv"))
-
-  dplyr::case_when(
-    disease == "covid" ~ "covid19-forecast-hub",
-    disease == "rsv" ~ "rsv-forecast-hub"
-  )
-}
-
 #' Get GitHub repository name for a given disease.
 #'
 #' Converts disease identifier to corresponding GitHub repository name.
@@ -45,8 +27,8 @@ get_hub_repo_name <- function(disease) {
   checkmate::assert_scalar(disease)
   checkmate::assert_names(disease, subset.of = c("covid", "rsv"))
 
-  return(dplyr::case_when(
+  dplyr::case_when(
     disease == "covid" ~ "covid19-forecast-hub",
     disease == "rsv" ~ "rsv-forecast-hub"
-  ))
+  )
 }
