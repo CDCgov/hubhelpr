c579de3c4a07a2af7e4a076861ab37d353f619a5
O957 <127630341+O957@users.noreply.github.com>
2026-08-14 09:18:47 -0400

    test: add tests for rescale and prefix presence

--- tests/testthat/test_write_ref_date_summary.R
+++ tests/testthat/test_write_ref_date_summary.R
@@ -31,14 +31,22 @@ test_that("write_ref_date_summary_all includes designation and ensemble columns"
 
   expect_true(all(
     c(
-      "quantile_0.10",
-      "quantile_0.90",
-      "quantile_0.10_rounded",
-      "quantile_0.90_rounded",
+      "count_quantile_0.10",
+      "count_quantile_0.90",
+      "count_quantile_0.10_rounded",
+      "count_quantile_0.90_rounded",
+      "rate_quantile_0.10",
+      "rate_quantile_0.90",
+      "rate_quantile_0.10_rounded",
+      "rate_quantile_0.90_rounded",
       "designated_model",
       "ensemble_of_hub_models"
     ) %in%
       names(summary_data)
+  ))
+
+  expect_false(any(
+    stringr::str_detect(names(summary_data), "^quantile_|_per100k")
   ))
 
   ensemble_flags <- summary_data |>
@@ -63,4 +71,74 @@ test_that("write_ref_date_summary_all includes designation and ensemble columns"
     epi_autogp_flags$designated_model,
     expected_designation$designated
   )
+})
+
+test_that("write_ref_date_summary_all rate columns rescale counts by population", {
+  output_path <- write_ref_date_summary_all(
+    reference_date = "2026-04-11",
+    base_hub_path = example_cfa_hub,
+    hub_reports_path = withr::local_tempdir(),
+    disease = "covid",
+    output_format = "csv",
+    n_models_for_ens_reporting = 0
+  )
+
+  summary_data <- forecasttools::read_tabular(output_path) |>
+    dplyr::mutate(
+      location = forecasttools::us_location_recode(
+        .data$abbreviation,
+        "abbr",
+        "hub"
+      )
+    ) |>
+    dplyr::left_join(hubhelpr::population_data, by = "location")
+
+  hosp_data <- dplyr::filter(summary_data, .data$target_data_type == "hosp")
+  expect_gt(nrow(hosp_data), 0)
+  expect_equal(
+    hosp_data$rate_quantile_0.5,
+    hosp_data$count_quantile_0.5 /
+      as.numeric(hosp_data$population) *
+      100000
+  )
+  ed_data <- dplyr::filter(summary_data, .data$target_data_type == "prop_ed")
+  expect_gt(nrow(ed_data), 0)
+  expect_true(all(is.na(ed_data$rate_quantile_0.5)))
+  expect_true(all(is.na(ed_data$rate_quantile_0.5_rounded)))
+  expect_false(any(is.na(ed_data$count_quantile_0.5)))
+})
+
+test_that("write_ref_date_summary_ens uses count_/rate_ prefixed columns", {
+  output_path <- write_ref_date_summary_ens(
+    reference_date = "2026-04-11",
+    base_hub_path = example_cfa_hub,
+    hub_reports_path = withr::local_tempdir(),
+    disease = "covid",
+    output_format = "csv",
+    n_models_for_ens_reporting = 0
+  )
+
+  map_data <- forecasttools::read_tabular(output_path)
+
+  expect_true(all(
+    c(
+      "count_quantile_0.025",
+      "count_quantile_0.5",
+      "count_quantile_0.975",
+      "count_quantile_0.025_rounded",
+      "count_quantile_0.5_rounded",
+      "count_quantile_0.975_rounded",
+      "rate_quantile_0.025",
+      "rate_quantile_0.5",
+      "rate_quantile_0.975",
+      "rate_quantile_0.025_rounded",
+      "rate_quantile_0.5_rounded",
+      "rate_quantile_0.975_rounded"
+    ) %in%
+      names(map_data)
+  ))
+
+  expect_false(any(
+    stringr::str_detect(names(map_data), "^quantile_|_per100k")
+  ))
 })
